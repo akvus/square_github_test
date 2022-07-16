414794176ec3c540fdad0e290a68d161b45fb644
akvus <mr.akvus@gmail.com>
2022-07-16 14:53:02 +0200

    Moving files around

--- app/src/main/kotlin/com/waysnpaths/github/common/view/serviceLocator/appModule.kt
+++ app/src/main/kotlin/com/waysnpaths/github/common/view/serviceLocator/appModule.kt
@@ -3,7 +3,6 @@ package com.waysnpaths.github.ui.serviceLocator
 import com.waysnpaths.github.ui.routing.Router
 import org.koin.dsl.module.module
 
-
 val appModule = module {
     factory {
         Router
